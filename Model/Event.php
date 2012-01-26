1a3c4dbb4c08fb3827fe75863da998d9675272d1
Joey jan <overflow636@gmail.com>
2012-01-26 15:48:39 -0600

    initial 2.x convert

---
+++ Model/Event.php
@@ -0,0 +1,37 @@
+<?php
+/*
+ * models/event.php
+ * CakePHP Full Calendar Plugin
+ *
+ * Copyright (c) 2010 Silas Montgomery
+ * http://silasmontgomery.com
+ *
+ * Licensed under MIT
+ * http://www.opensource.org/licenses/mit-license.php
+ */
+ 
+class Event extends FullCalendarAppModel {
+	var $name = 'Event';
+	var $displayField = 'title';
+	var $validate = array(
+		'title' => array(
+			'notempty' => array(
+				'rule' => array('notempty'),
+			),
+		),
+		'start' => array(
+			'notempty' => array(
+				'rule' => array('notempty'),
+			),
+		)
+	);
+
+	var $belongsTo = array(
+		'EventType' => array(
+			'className' => 'FullCalendar.EventType',
+			'foreignKey' => 'event_type_id'
+		)
+	);
+
+}
+?>
